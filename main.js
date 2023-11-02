9f549252ef82f3245c33e699144f8d739a688402
yusufNinj1 <142718760+yusufNinj1@users.noreply.github.com>
2023-11-02 15:51:34 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+function setup() {
+    canvas=createCanvas(550, 510);
+    canvas.position(560, 120);
+
+    video = createCapture(VIDEO);
+    video.size(550, 500);
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function draw() {
+    background('red');
+}
+
+function modelLoaded() {
+    console.log('PoseNet Is Initialized!');
+}
+
+function gotPoses(results)
+{
+    if(results.length > 0)
+    {
+        console.log(results);
+    }
+}
